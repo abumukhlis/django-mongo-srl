670902b7259703c0140bf68c2470fee2e5c262c9
Azeez Lawal <codewithazeez@gmail.com>
2021-09-15 00:37:51 +0100

    made changes to cards and made migrations

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cards/', include('apps.cards.urls')),
+    # path('decks/', include('apps.decks.urls'))
     path('', include('rest_framework.urls')),
 ]
